259516b50bc9504b6a1e7a9933fe625dc8f3b426
Rohit Sharma <54910136+jason7531@users.noreply.github.com>
2020-05-15 18:40:53 +0530

    Add files via upload

---
+++ while.js
@@ -0,0 +1,26 @@
+let num1 = -10;
+while (num1 <= 19) {
+    console.log(num1);
+    num1++;
+}
+let num2 = 10;
+while (num2 <= 40) {
+    if (num2 % 2 === 0) {
+        console.log(num2)
+    }
+    num2++;
+}
+let num3 = 300;
+while (num3 <= 333) {
+    if (num3 % 2 != 0) {
+        console.log(num3)
+    }
+    num3++;
+}
+let num4 = 5;
+while (num4 <= 50) {
+    if (num4 % 3 === 0 && num4 % 5 === 0) {
+        console.log(num4)
+    }
+    num4++;
+}
